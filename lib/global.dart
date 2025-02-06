7c614620e632b6351425073f866b576f2e896e07
JIEEEN <tjtpp009@korea.ac.kr>
2025-02-06 16:28:35 +0900

    mod mainColor

--- lib/global.dart
+++ lib/global.dart
@@ -1,7 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-const Color mainColor = Color.fromARGB(255, 196, 48, 43);
+const Color mainColor = Color.fromARGB(255, 205, 20, 31);
 const Color whiteBackgroundColor = Colors.white;
 const Color blackBackgroundColor = Colors.black54;
 const Color noticeWidgetColor = Color.fromARGB(255, 237, 242, 243);
