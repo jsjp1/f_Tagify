b12aa7b3e0175a15d6e9f4f6226cbf68c56ed156
JIEEEN <tjtpp009@korea.ac.kr>
2025-02-11 17:13:23 +0900

    add few global config

--- lib/global.dart
+++ lib/global.dart
@@ -7,19 +7,22 @@ const Color blackBackgroundColor = Colors.black54;
 const Color noticeWidgetColor = Color.fromARGB(255, 240, 243, 243);
 const Color videoTextBarColor = Color.fromARGB(255, 228, 228, 228);
 const Color timeContainerColor = Color.fromARGB(200, 225, 225, 225);
+const Color tagColor = Color.fromARGB(255, 50, 50, 50);
 
 class GlobalText extends StatelessWidget {
   final String localizeText;
   final double textSize;
   final bool isBold;
   final Color textColor;
+  final TextOverflow overflow;
 
   const GlobalText(
       {super.key,
       required this.localizeText,
       required this.textSize,
       required this.isBold,
-      required this.textColor});
+      required this.textColor,
+      required this.overflow});
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +33,7 @@ class GlobalText extends StatelessWidget {
         fontSize: textSize,
         fontFamily: "YoutubeFont",
         fontWeight: isBold ? FontWeight.bold : null,
+        overflow: overflow,
       ),
     ).tr();
   }
